4305ce5a9a3328c3d41df65b755d5c27fa772d9b
OlarindeRamat <169068889+Hibiikay@users.noreply.github.com>
2026-08-01 17:00:39 +0100

    Add Balloon.js with resizing functionality

---
+++ Balloon.js
@@ -0,0 +1,28 @@
+<p>🎈</p>
+
+<script>
+  let balloon = document.querySelector("p");
+  let size = 20;
+
+  balloon.style.fontSize = size + "px";
+
+  function resize(event) {
+    if (event.key === "ArrowUp") {
+      event.preventDefault();
+      size *= 1.1;
+
+      if (size > 100) {
+        balloon.textContent = "💥";
+        window.removeEventListener("keydown", resize);
+      } else {
+        balloon.style.fontSize = size + "px";
+      }
+    } else if (event.key === "ArrowDown") {
+      event.preventDefault();
+      size *= 0.9;
+      balloon.style.fontSize = size + "px";
+    }
+  }
+
+  window.addEventListener("keydown", resize);
+</script>
